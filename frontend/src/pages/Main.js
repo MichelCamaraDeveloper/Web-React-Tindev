d9876dbb0eef8f48c5b30526b2c142ec2ca06e6c
Michel Câmara <michelvictor16@gmail.com>
2019-08-28 12:22:11 -0400

    Desenvolvimento web front Tindev
    Nessa submissão foi realizada:
    - Funções like e dislike;
    - Renderização quando for dado like e dislike;
    - Verificação da lista de usuários;

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,80 +1,79 @@
-import React from 'react';
-
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
 import  './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 
 export default function Main({ match }){
+    const[users, setUsers] = useState([]);
+
+    useEffect(()=> {
+        async function loadUsers(){
+            const response = await api.get('/devs', {
+                headers:{
+                    user:match.params.id,
+                }
+            })
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [match.params.id]);
+
+    async function handleLike(id){
+        await api.post(`/devs/${id}/likes`, null, {
+            headers : {user: match.params.id},
+        })
+
+        // Chama a função setUsers para filtrar e mostrar na tela somente usuários cujo id é diferente do que
+        // foi passado no parametro
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers : {user: match.params.id},
+        })
+
+        // Chama a função setUsers para filtrar e mostrar na tela somente usuários cujo id é diferente do que
+        // foi passado no parametro
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return(
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
-            <ul>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/21249661?v=4" alt="" />
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
+            {/* Se o tamanho da lista for maior que zero então mostra os conteudos
+            se não mostra uma mensagem */}
+            {users.length > 0 ?(
+                <ul>
+                {/* Percorrendo a array de users */}
+                {users.map(user =>(
+                    <li key={user._id}>
+                    <img src={user.avatar} alt={user.name} />
                     <footer>
-                        <strong>Programador</strong>
-                        <p>Uma breve descrição do Programador acima. </p>
+                        <strong>{user.name}</strong>
+                        <p>{user.bio}</p>
                     </footer>
                     <div className="buttons">
-                        <button type="button">
+                        <button type="button" onClick={()=> handleDislike(user._id)}>
                             <img src={dislike} alt="Dislike" />
                         </button>
-                        <button type="button">
+                        <button type="button" onClick={()=> handleLike(user._id)}>
                             <img src={like} alt="Like" />
                         </button>
                     </div> 
                 </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/21249661?v=4" alt="" />
-                    <footer>
-                        <strong>Programador</strong>
-                        <p>Uma breve descrição do Programador acima. </p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div> 
-                </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/21249661?v=4" alt="" />
-                    <footer>
-                        <strong>Programador</strong>
-                        <p>Uma breve descrição do Programador acima. </p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div> 
-                </li>
-
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/21249661?v=4" alt="" />
-                    <footer>
-                        <strong>Programador</strong>
-                        <p>Uma breve descrição do Programador acima. </p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div> 
-                </li>
+                ))}
             </ul>
+            ):(
+                <div className="empty"> Acabou :( </div>
+            )}
         </div>
 
     );
